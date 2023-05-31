fd0a8ad276da565f96a1e192c5d0d5cba97fe2c3
Frederik Karlsson <frederikkarlsson@me.com>
2023-05-31 20:16:30 +0200

    adding a graph

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import frontend
+import networkx as nx
+from matplotlib import pyplot as plt
 from fastapi import FastAPI
 from kafka import KafkaConsumer
 import datamodel
@@ -68,6 +70,37 @@ def init(app: FastAPI) -> None:
 
                     ui.label('Height:')
                     ui.label('1.80m')
+    @ui.page('/graph')
+    def some_graph():
+
+        with ui.pyplot(figsize=(10, 5)):
+                    
+                    G = nx.Graph()
+                    G.add_edge(1, 2)
+                    G.add_edge(1, 3)
+                    G.add_edge(1, 5)
+                    G.add_edge(2, 3)
+                    G.add_edge(3, 4)
+                    G.add_edge(4, 5)
+
+                    # explicitly set positions
+                    pos = {1: (0, 0), 2: (-1, 0.3), 3: (2, 0.17), 4: (4, 0.255), 5: (5, 0.03)}
+
+                    options = {
+                        "font_size": 36,
+                        "node_size": 3000,
+                        "node_color": "white",
+                        "edgecolors": "black",
+                        "linewidths": 5,
+                        "width": 5,
+                    }
+                    nx.draw_networkx(G, pos, **options)
+
+                    # Set margins for the axes so that nodes aren't clipped
+                    ax = plt.gca()
+                    ax.margins(0.20)
+                    plt.axis("off")
+            
 
     ui.run_with(app)
 
