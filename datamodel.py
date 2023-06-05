76eb3a5e328355ce34bb699a7f4449b0e34b866b
Frederik Karlsson <frederikkarlsson@me.com>
2023-06-05 08:49:14 +0200

    cleanup

--- datamodel.py
+++ datamodel.py
@@ -1,24 +1,10 @@
 import threading
-from lib import ThreadSafeDictionary
+from model_lib import ThreadSafeDictionary
 import time
 
 # Create an instance of the thread-safe dictionary with Kafka consumer
 subscribers = ThreadSafeDictionary('acs_subscribers', ['localhost:9092'])
 
 # Create another instance of the thread-safe dictionary with Kafka consumer
-# devices = ThreadSafeDictionary('acs_devices', ['localhost:9092'])
+graphtest = ThreadSafeDictionary('graph_test', ['localhost:9092'])
 
-# Function to print the dictionary items
-# def print_items():
-#     while True:
-#         items = somedict.items()
-#         print(items)
-#         time.sleep(2)
-#         # Add sleep or other logic here if needed
-
-# # Create a thread to print the dictionary items
-# print_thread = threading.Thread(target=print_items)
-# print_thread.start()
-
-# # Wait for threads to finish
-# print_thread.join()
--- main.py
+++ main.py
@@ -70,21 +70,23 @@ def init(app: FastAPI) -> None:
 
                     ui.label('Height:')
                     ui.label('1.80m')
+
     @ui.page('/graph')
     def some_graph():
-
         with ui.pyplot(figsize=(10, 5)):
                     
                     G = nx.Graph()
                     G.add_edge(1, 2)
                     G.add_edge(1, 3)
-                    G.add_edge(1, 5)
+                    G.add_edge(1, datamodel.graphtest['test']['one'])
                     G.add_edge(2, 3)
                     G.add_edge(3, 4)
+                    
                     G.add_edge(4, 5)
 
+
                     # explicitly set positions
-                    pos = {1: (0, 0), 2: (-1, 0.3), 3: (2, 0.17), 4: (4, 0.255), 5: (5, 0.03)}
+                    # pos = {1: (0, 0), 2: (-1, 0.3), 3: (2, 0.17), 4: (4, 0.255), 5: (5, 0.03)}
 
                     options = {
                         "font_size": 36,
@@ -94,14 +96,14 @@ def init(app: FastAPI) -> None:
                         "linewidths": 5,
                         "width": 5,
                     }
-                    nx.draw_networkx(G, pos, **options)
+                    nx.draw_networkx(G, **options)
 
                     # Set margins for the axes so that nodes aren't clipped
                     ax = plt.gca()
                     ax.margins(0.20)
                     plt.axis("off")
-            
-
+    ui.timer(1.0, some_graph)
+    # https://networkx.org/documentation/stable/auto_examples/drawing/plot_custom_node_icons.html#sphx-glr-auto-examples-drawing-plot-custom-node-icons-py
     ui.run_with(app)
 
 
